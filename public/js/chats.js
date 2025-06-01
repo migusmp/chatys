32b2d6f67b6a948e15477773d0a5bff9cc5fe65f
migusmp <mmp2004sp@gmail.com>
2025-06-01 17:48:07 +0000

    friends page working as a SPA

--- public/js/chats.js
+++ public/js/chats.js
@@ -8,33 +8,37 @@ import { goto } from "./router.js";
 let generalStatsSocket = null;
 
 export function initPage() {
-    document.querySelector('.username').textContent = GlobalState.get('username');
+  document.querySelector('.username').textContent = GlobalState.get('username');
 
-    const roomInput = document.getElementById("roomNameInput");
-    document.getElementById("joinRoomBtn").addEventListener("click", () => {
-      if (!roomInput.value.trim()) {
-        alert("Por favor, ingresa un nombre de sala.");
-        return;
+  document.querySelector(".friends").addEventListener("click", () => {
+    goto("/friends");
+  })
+
+  const roomInput = document.getElementById("roomNameInput");
+  document.getElementById("joinRoomBtn").addEventListener("click", () => {
+    if (!roomInput.value.trim()) {
+      alert("Por favor, ingresa un nombre de sala.");
+      return;
     }
     goto(`/chats/${roomInput.value.trim()}`);
     roomInput.value = ""; // Limpiar el input después de unirse
 
-    })
-    // Crear la conexión y asignarla a la variable
-    generalStatsSocket = WSChat.connectGeneralStats((stats) => {
-      renderChatStats(stats);
-      renderActiveRooms(stats.activeRooms);
-    });
-    GlobalState.initSocket();
-    initNotifications();
-    initProfileMenu();
+  })
+  // Crear la conexión y asignarla a la variable
+  generalStatsSocket = WSChat.connectGeneralStats((stats) => {
+    renderChatStats(stats);
+    renderActiveRooms(stats.activeRooms);
+  });
+  GlobalState.initSocket();
+  initNotifications();
+  initProfileMenu();
+}
+
+export function destroyPage() {
+  if (generalStatsSocket) {
+    generalStatsSocket.close();  // Cierra el WebSocket
+    generalStatsSocket = null;
   }
-  
-  export function destroyPage() {
-    if (generalStatsSocket) {
-      generalStatsSocket.close();  // Cierra el WebSocket
-      generalStatsSocket = null;
-    }
-    destroyNotifications();
-  }
+  destroyNotifications();
+}
 
